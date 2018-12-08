e51436b209932bc62e7132c57b09aada7e8f2f02
“ZhuyuLi” <zhuyulicfc@gmail.com>
2018-12-08 18:21:51 -0500

    refactor of logical structure for mainfragment

---
+++ PHX-Final/app/src/main/java/com/example/zhuyu/phx_final/FeedbackActivity.java
@@ -0,0 +1,65 @@
+package com.example.zhuyu.phx_final;
+
+import android.os.Bundle;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
+public class FeedbackActivity extends AppCompatActivity {
+
+    private TextView mContactUsTextView;
+    private TextView mNameTextView;
+    private TextView mEmailTextView;
+    private TextView mOrganizationTextView;
+    private TextView mQuestionTextView;
+
+
+    private EditText mNameEditText;
+    private EditText mEmailEditText;
+    private EditText mOrganizationEditText;
+    private EditText mQuestionEditText;
+    private Button mSubmitButton;
+
+    private static final String TAG = "MailSenderActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.feedback_activity);
+
+        mContactUsTextView = findViewById(R.id.contact_us_text_view);
+        mNameTextView = findViewById(R.id.name_text_view);
+        mEmailTextView = findViewById(R.id.email_text_view);
+        mOrganizationTextView = findViewById(R.id.organization_text_view);
+        mQuestionTextView = findViewById(R.id.question_text_view);
+
+
+        mNameEditText = findViewById(R.id.name_edit_text);
+        mEmailEditText = findViewById(R.id.email_edit_text);
+        mOrganizationEditText = findViewById(R.id.organization_edit_text);
+        mQuestionEditText = findViewById(R.id.question_edit_text);
+
+        mSubmitButton = findViewById(R.id.submit_button);
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msgToSend = "Feedback Info:\n\n" + "Name: " + mNameEditText.getText().toString()
+                        + "\n\nEmail: " + mEmailEditText.getText().toString() + "\n\nOrganization: " + mOrganizationEditText.getText().toString() + "\n\nQuestion/Comment: " + mQuestionEditText.getText().toString();
+                MailSenderUtils mailSenderUtils = new MailSenderUtils("FeedBack", msgToSend);
+                mailSenderUtils.sendEmail();
+            }
+        });
+    }
+
+}
--- PHX-Final/app/src/main/java/com/example/zhuyu/phx_final/MainFragment.java
+++ PHX-Final/app/src/main/java/com/example/zhuyu/phx_final/MainFragment.java
@@ -1,13 +1,17 @@
 package com.example.zhuyu.phx_final;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,10 +21,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-public class MainFragment extends Fragment implements BottomNavigationView.OnNavigationItemSelectedListener, ViewPager.OnPageChangeListener{
+public class MainFragment extends Fragment implements BottomNavigationView.OnNavigationItemSelectedListener, ViewPager.OnPageChangeListener, NavigationView.OnNavigationItemSelectedListener{
 
     private BottomNavigationView mBottomNavigationView;
+    private NavigationView mSlideNavigationView;
     private ViewPager mViewPager;
+    private DrawerLayout mDrawerLayout;
     private static final String TAG = "MainFragment";
 
     @Override
@@ -32,9 +38,12 @@ public class MainFragment extends Fragment implements BottomNavigationView.OnNav
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.main_main, container, false);
         mBottomNavigationView = v.findViewById(R.id.main_bottom_navigation_view);
+        mSlideNavigationView = v.findViewById(R.id.mine_navi_view);
         mViewPager = v.findViewById(R.id.main_view_pager);
+        mDrawerLayout = v.findViewById(R.id.mine_drawer_layout);
 
         mBottomNavigationView.setOnNavigationItemSelectedListener(this);
+        mSlideNavigationView.setNavigationItemSelectedListener(this);
 
         mViewPager.addOnPageChangeListener(this);
         FragmentManager fragmentManager = getChildFragmentManager();
@@ -57,10 +66,22 @@ public class MainFragment extends Fragment implements BottomNavigationView.OnNav
             case R.id.item_download:
                 clickDownload();
                 return true;
+            case R.id.item_mine:
+                if (mDrawerLayout.isDrawerOpen(GravityCompat.END)) {
+                    mDrawerLayout.closeDrawer(GravityCompat.END);
+                } else {
+                    mDrawerLayout.openDrawer(GravityCompat.END);
+                }
+                return true;
+            case R.id.slide_navi_feedback:
+                Intent intent = new Intent(getActivity(), FeedbackActivity.class);
+                startActivity(intent);
+                mDrawerLayout.closeDrawer(GravityCompat.END);
+                return true;
             default:
                     break;
         }
-        return false;
+        return true;
     }
 
     @Override
@@ -108,7 +129,7 @@ public class MainFragment extends Fragment implements BottomNavigationView.OnNav
 
         private Fragment[] mFragments = new Fragment[4];
 
-        public ViewPagerAdapter(Fragment f1, Fragment f2, Fragment f3, FragmentManager fm) {
+        private ViewPagerAdapter(Fragment f1, Fragment f2, Fragment f3, FragmentManager fm) {
             super(fm);
             mFragments[0] = f1;
             mFragments[1] = f2;
